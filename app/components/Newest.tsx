029d3735daad495614b4f72ede17b1cc02958148
Muhammad Hassan <155459839+muhammad-hassn@users.noreply.github.com>
2024-10-20 14:13:29 +0500

    Final Commit

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -19,6 +19,8 @@ async function GetData() {
   return data;
 }
 
+export const dynamic = 'force-dynamic'
+
 const Newest = async () => {
   const data: simplifiedProduct[] = await GetData();
   return (
