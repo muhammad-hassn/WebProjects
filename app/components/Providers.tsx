8ba44076ce7836b70eef77b5aa91e52656b6bba5
Muhammad Hassan <155459839+muhammad-hassn@users.noreply.github.com>
2024-11-27 17:19:47 +0500

    Update Providers.tsx

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -7,8 +7,8 @@ return(
 mode="payment" 
 cartMode="client-only"
 stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string }
-successUrl="https://nextcommerce-coral.vercel.app/stripe/sucess"
-cancelUrl="https://nextcommerce-coral.vercel.app/stripe/error"
+successUrl="https://nextcommerce-ten-hazel.vercel.app/stripe/sucess"
+cancelUrl="https://nextcommerce-ten-hazel.vercel.app/stripe/error"
 currency="USD"
 billingAddressCollection={false}
 shouldPersist={true}
